f709526819e236338ef93d5a4e485ca3199d4b8b
AJMCGamer <alexanderp.janvier12@gmail.com>
2025-01-11 16:39:50 -0500

    Test

---
+++ src/main/java/net/ajmc/geneticraft/GenetiCraft.java
@@ -0,0 +1,16 @@
+package net.ajmc.geneticraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GenetiCraft implements ModInitializer {
+	public static final String MOD_ID = "geneticraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/ajmc/geneticraft/GenetiCraftClient.java
@@ -0,0 +1,10 @@
+package net.ajmc.geneticraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GenetiCraftClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
