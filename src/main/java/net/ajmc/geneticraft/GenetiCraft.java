b11f3c0fcf72540112e35d98b3408d4670e088cf
AJMCGamer <alexanderp.janvier12@gmail.com>
2025-01-13 12:18:04 -0500

    Added Amber

--- src/main/java/net/ajmc/geneticraft/GenetiCraft.java
+++ src/main/java/net/ajmc/geneticraft/GenetiCraft.java
@@ -1,16 +1,17 @@
 package net.ajmc.geneticraft;
 
+import net.ajmc.geneticraft.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//very important!!!!!!!!!!!!!!!
+
 public class GenetiCraft implements ModInitializer {
 	public static final String MOD_ID = "geneticraft";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registeredModItems();
 	}
 }
---
+++ src/main/java/net/ajmc/geneticraft/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.ajmc.geneticraft.item;
+
+import net.ajmc.geneticraft.GenetiCraft;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item AMBER = registerItem("amber", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(GenetiCraft.MOD_ID,"amber")))));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(GenetiCraft.MOD_ID, name), item);
+    }
+
+    public static void registeredModItems(){
+        GenetiCraft.LOGGER.info("Registering Mod Items for" + GenetiCraft.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(AMBER);
+        });
+    }
+}
